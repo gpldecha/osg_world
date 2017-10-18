c71a88c193ae1b860cba81178805e41a7eca449c
Guillaume de Chambrier <chambrierg@gmail.com>
2017-10-18 17:49:32 +0100

    first commit

---
+++ KeyboardHandler.h
@@ -0,0 +1,43 @@
+#ifndef KEYBOARD_HANDLER_H
+#define KEYBOARD_HANDLER_H
+#include <iostream>
+#include <osgGA/GUIEventHandler>
+
+class keyboardEventHandler : public osgGA::GUIEventHandler
+{
+public:
+
+	typedef void (*functionType) ();
+
+   enum keyStatusType
+   {
+      KEY_UP, KEY_DOWN 
+   };
+
+   struct functionStatusType
+   {
+      functionStatusType() {keyState = KEY_UP; keyFunction = NULL;}
+      functionType keyFunction;
+      keyStatusType keyState;
+   };
+
+   bool addFunction(int whatKey, functionType newFunction);
+
+   bool addFunction(int whatKey, keyStatusType keyPressStatus, functionType newFunction);
+
+   virtual bool handle(const osgGA::GUIEventAdapter& ea,osgGA::GUIActionAdapter&);
+
+   virtual void accept(osgGA::GUIEventHandler& v){
+       std::cout<< "accept event" << std::endl;
+       /*v. v.visit(*this); */};
+
+protected:
+
+	typedef std::map<int, functionStatusType > keyFunctionMap;
+   keyFunctionMap keyFuncMap;
+   keyFunctionMap keyUPFuncMap;
+
+};
+
+#endif
+
